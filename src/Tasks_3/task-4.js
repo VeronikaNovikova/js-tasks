377ed5cca932a1e6d3e397497582f6ec24fd0f08
vnovikova <veronikanovikova@coherentsolutions.com>
2018-11-05 11:37:48 +0300

    add union function

---
+++ src/Tasks_3/task-4.js
@@ -0,0 +1,18 @@
+(function() {
+  function union(...arrays) {
+    let a = [],
+        obj = {};
+
+    for (let i = 0; i < arrays.length; i++) {
+      a = a.concat(arrays[i]);
+    }
+
+    for (let i = 0; i < a.length; i++) {
+      let item = a[i];
+      obj[item] = true;
+    }
+    return Object.keys(obj);
+  }
+
+  alert(union([1, 2, 2, 3], [101, 2, 1, 10], [2, 1]) );
+})();
